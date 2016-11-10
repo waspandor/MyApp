695a6f11e701e8e5e0bd2e633b9ae1194ff33f32
Waseem <waseempandor@gmail.com>
2016-11-10 21:51:01 +0000

    carpark

--- src/main/java/latest/CarParkInterface.java
+++ src/main/java/latest/CarParkInterface.java
@@ -8,17 +8,13 @@ public class CarParkInterface {
     public static void main(String[] args) {
 
         CarPark park = new CarPark();
-        Vehicle car = new Vehicle();
 
-        park.checkAndParkIfSpace(car, "a", 2);
-        park.checkAndParkIfSpace(car, "b", 2);
-        park.checkAndParkIfSpace(car, "c", 2);
-        park.checkAndParkIfSpace(car, "d", 2);
-        park.checkAndParkIfSpace(car, "d", 2);
-        park.checkAndParkIfSpace(car, "d", 1);
-        park.checkAndParkIfSpace(car, "d", 6);
+        park.checkAndParkIfSpace("calica", 2);
+        park.checkAndParkIfSpace("toyota", 1);
 
+        System.out.println("Vehicle Details");
         park.carDetails();
+
 
 
     }
